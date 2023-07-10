9f65a5fe51111259b3095dfd106206ac9e1d78b1
RufetYusubov <rufetyusubov20gmail.com>
2023-07-10 23:11:02 +0400

    Deyisikler edildi

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -3,7 +3,8 @@ from BlogApp import views
 
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name="index"),
-    path('category/<int:id>',views.CategoryView.as_view(),name="categorylink"),
+    path('category/<int:id>/',views.CategoryView.as_view(),name="categorylink"),
+    path('tagcloud/<int:id>/',views.TagCloudView.as_view(),name="tagcloud"),
     path('detail/<int:id>',views.PostDetailView.as_view(),name="post-details"),
     path('about/',views.AboutView.as_view(),name="about"),
 ]
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -21,15 +21,29 @@ class CategoryView(View):
     def get(self,request,id,*args,**kwargs):
         category = CategoryModel.objects.get(id=id)
         categories = CategoryModel.objects.all()
-        blog = BlogModel.objects.get(id=id)
+        blogs = BlogModel.objects.filter(category=category)
 
         context = {
             'category' : category,
             'categories' : categories,
-            'blog' : blog
+            'blogs' : blogs
         }
 
         return render(request,"categories.html",context)
+#-------------------------------------------------------------------------------------------------
+class TagCloudView(View):
+    def get(self,request,id,*args,**kwargs):
+        tagcloud = TagCloudModel.objects.get(id=id),
+        tagclouds = TagCloudModel.objects.all()
+        blogs = BlogModel.objects.filter(tag_cloud=tagcloud)
+
+        context = {
+            'tagcloud' : tagcloud,
+            'tagclouds' : tagclouds,
+            'blogs' : blogs
+        }
+
+        return render(request,'tagcloud.html',context)
 #--------------------------------------------------------------------------------------------------
 class PostDetailView(View):
     def get(self,request,id,*args,**kwargs):
